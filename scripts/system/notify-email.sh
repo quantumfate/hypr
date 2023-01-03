a85fd78ce596f1cd4772e3dc62c7f28ca8666c3a
Leon Connor Holm <leonconnorholm@gmail.com>
2023-01-03 16:23:12 +0100

    moved icons, implemented email notify

--- scripts/system/notify-email.sh
+++ scripts/system/notify-email.sh
@@ -4,8 +4,13 @@
 
 iDIR="$HOME/.config/hypr/icons"
 
-notify_cmd_shot="notify-send -h string:x-canonical-private-synchronous:shot-notify -u low -i ${iDIR}/mail.png"
+cut_cmd="cut -c1-99"
+notify_cmd_base="notify-send" 
+notify_category="Mail"
+notify_cmd_hint="string:x-canonical-private-synchronous:shot-notify"
+notify_cmd_icon="${iDIR}/mail.png"
 
+YELLOW='\e[33m'
 log_message() {
     # Takes a message and variable as parameter
     local log_dir="$HOME/.local/log"
@@ -17,7 +22,23 @@ log_message() {
     fi
 
     time_stamp=$(date +"%Y-%m-%d %H:%M:%S")
-    echo "$time_stamp : $1" >> "$log_dir/$log_file"
+    echo -e "$time_stamp : $1" >> "$log_dir/$log_file"
+}
+
+cut_string() {
+    # Cut the string and return [...]
+    # To ensure that the style of the notification doesn't break
+
+    local cut_part=""
+    cut_part=$($cut_cmd <<< "$1")
+    if [[ "${#cut_part}" -lt "${#1}" ]]; then
+        # return the the smaller sliced part
+        echo "$cut_part \[...\]"
+        return
+    fi
+    # no cut needed and return the initial argument
+    echo "$1"
+    
 }
 
 # Initialize variable to store option
@@ -40,82 +61,95 @@ message_is_flagged=false # Whether the email is flagged or not
 message_is_new=false # Whether the email is new or not (e.g. moved between folders)
 message_is_deleted=false # Whether the email was deleted
 
+new_recipient=false # Whether to reply to the sender or the new recipient
+
 opts="a:n:r:A:T:R:s:D:Z:"
 # Parse options
 while getopts "$opts" opt; do
   case "$opt" in
     a) 
         # Sender address %a
-        echo $OPTARG
         sender_address="$OPTARG"
         ;;
     n)
         # Sender name %n or %a if none
-        echo $OPTARG
         sender_name="$OPTARG"        
         ;;
     r)
-        # Sender name %n or %a if none
-        echo $OPTARG
+        # List of recipients %r
         receive_list="$OPTARG"        
         ;;
 
     A)
         # Reply-to address %A or %a if none
-        echo $OPTARG
         reply_to_address="$OPTARG"
         ;;
     T)
         # Receiver adress (Email account) %T
-        echo $OPTARG
         user_account_address="$OPTARG"
         ;;
     R)
         # CC-List %R (comma separated)
-        echo $OPTARG
         cc_list="$OPTARG"
         ;;
     s)
         # Subject %s
-        echo $OPTARG
         email_subject="$OPTARG"
         ;;
     D)
         # Local Time %D
-        echo $OPTARG
         local_system_time="$OPTARG"
         ;;
     Z)
         # Set the default notification urgency to low
-        echo $OPTARG
         flags="$OPTARG"
-        notification_urgencylow="low"
+        notification_urgency="low"
 
-        if [[ "$OPTARG" == *O* ]] || [[ "$OPTARG" == *N* ]]; then
+        if [[ "$OPTARG" == *N* ]]; then
             # Handle old or new mails
-            message_is_old=true
             message_is_new=true
             notification_urgency="low"
-        elif [[ "$OPTARG" == *D* ]]; then
+        fi
+        
+        if [[ "$OPTARG" == *O* ]]; then
+            message_is_old=true
+            notification_urgency="low"
+        fi
+
+        if [[ "$OPTARG" == *D* ]]; then
             # Handle deleted mails
             message_is_deleted=true
             notification_urgency="low"
-        elif [[ "$OPTARG" == *r* ]]; then
+        fi
+
+        if [[ "$OPTARG" == *r* ]]; then
             # Handle answered mails
             message_is_response=true
-            notification_urgency="medium"
-        elif [[ "$OPTARG" == *!* ]]; then
+            notification_urgency="normal"
+        fi
+
+        if [[ "$OPTARG" == *!* ]]; then
             # Handle flagged mails
             message_is_flagged=true
-            notification_urgency="high"
-        elif [[ "$OPTARG" == *\** ]]; then
+            notification_urgency="critical"
+        fi
+
+        if [[ "$OPTARG" == *\** ]]; then
             # Handle marked mails
             message_is_marked=true
-            notification_urgency="high"
-        else
-          # Handle all other mails
-          log_message "WARNING! Invalid flag in the %Z option found: '$OPTARG'! This flag was ignored."
+            notification_urgency="critical"
         fi
+        
+        supported_flags='rNOD!*'
+        # unsupported flags
+        for i in ${flags//,/ }
+        do
+            # Flag is unsupported -> ignored
+            if [[ "$supported_flags" != *$i* ]]; then
+                log_message "${YELLOW}WARNING!\e[0m Invalid flag in the %Z option found: '$i'! This flag was ignored. Input was: '$OPTARG'"
+            fi
+
+        done
         ;;
     \?)
         log_message "Invalid option: -$OPTARG"
@@ -133,7 +167,7 @@ email_is_from=""
 if [[ "$sender_address" == "$sender_name" ]]; then
     email_is_from+="$sender_address"
 else
-    email_is_from+="$sender_name\033[3m($sender_address)\033[0m"
+    email_is_from+="$sender_name\[$sender_address\]"
 fi
 
 reply_to=""
@@ -141,57 +175,68 @@ reply_to=""
 if [[ "$sender_address" == "$reply_to_address" ]]; then
     reply_to+="$sender_address"
 else
+    new_recipient=true
     reply_to+="$reply_to_address"
 fi
 
-notify_string=""
+notify_title=""
+notify_body=""
 
 if [[ "$message_is_new" == "true" ]]; then
-    echo "test"
     
-    if [[ "$message_is_deleted" == "true" ]] \
-        && [[ "$message_is_flagged" == "true" || "$message_is_marked" == "true" ]]; then
-    
-        notify_string="\033[1mWarning!\033[0m A new E-Mail with high priority got deleted!\n\n"
-    else
-        notify_string+="New "
+    if [[ "$message_is_deleted" == "true" ]]; then
         if [[ "$message_is_flagged" == "true" || "$message_is_marked" == "true" ]]; then
-            notify_string+=" prioritized "
+            notify_title+="Warning! A new E-Mail with high priority got deleted!"
+        else
+            notify_title+="Warning! A new E-Mail got deleted!"
         fi
-        notify_string+="E-Mail arrived!\n\n"
+    else
+        notify_title+="New "
+        if [[ "$message_is_flagged" == "true" || "$message_is_marked" == "true" ]]; then
+            notify_title+="prioritized "
+        fi
+        notify_title+="E-Mail arrived!"
     fi
 
-    notify_string+="\033[1m$user_account_address\033[0m "
+    notify_body+="\n$email_is_from "
 
     if [[ "$message_is_response" == "true" ]]; then
-        notify_string="replied to one of your E-Mails "
+        notify_body+="replied to one of your E-Mails"
     else
-        notify_string="sent you an E-Mail"
+        notify_body+="sent you an E-Mail"
     fi
 
     if [[ "$message_is_old" == "true" ]]; then
-        notify_string+=" while you were offline.n\n"
+        notify_body+=" while you were offline."
     else
-        notify_string+=".\n"
+        notify_body+="."
     fi
     
-    notify_string+="\033[1mFrom:\033[0m $email_is_from\n"
+    notify_body+="\n\n<b>From:</b> $email_is_from\n"
 
     # If the email_is_from received an email because they were cc'd
-    if [[ $receive_list != *$user_account_address* ]] && [[  $cc_list == *$user_account_address* ]]; then
-        notify_string+="Information: You are listed in the CC of the E-Mail\n"
-    elif [[ $cc_list == *$user_account_address* ]]; then
+    if [[ "$receive_list" != *$user_account_address* ]] && [[  "$cc_list" == *$user_account_address* ]]; then
+        notify_body+="<b>Information:</b> You were mentioned in CC.\n"
+    elif [[ "$cc_list" == *$user_account_address* ]]; then
         # The reveiver can be in reply-to and in cc
-        notify_string+="Information: You will receive this E-Mail as a copy in CC\n"
+        notify_body+="<b>Information:</b> You received this E-Mail as a copy in CC.\n"
     fi
 
-    notify_string+="\033[1mSubject:\033[0m '$email_subject.'\n"
+    notify_body+="<b>Subject:</b> $(cut_string "$email_subject").\n"
 
-    notify_string+="Reply to $reply_to to continue the conversation\n"
-    
-    notify_string+="\033[1mTime:\033[0m $local_system_time"
+    # Reply to will either be the sender or a new specified recipient
+    notify_body+="<b>Reply-To:</b> "
+    if [[ "$new_recipient" == "true" ]]; then
+        notify_body+="Reply to $reply_to to continue the conversation\n"
+    else
+        notify_body+="Reply to $email_is_from to continue the conversation\n"
+    fi
 
-    $notify_cmd_shot -u "$notification_urgency" "$notify_string"
+    notify_body+="<b>Time:</b> $local_system_time"
 
+    $notify_cmd_base -c "$notify_category" -h "$notify_cmd_hint" -u "$notification_urgency" -i "$notify_cmd_icon" "$notify_title" "$notify_body"
+
+else 
+    log_message "${YELLOW}WARNING! Unsupported behaviour detected.\e[0m"
 fi
 exit 0
