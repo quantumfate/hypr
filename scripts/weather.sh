fa4d9132b79a05cdbb311ab59ff352d9f6229084
Leon Connor Holm <leonconnorholm@gmail.com>
2024-07-09 14:00:51 +0200

    Log fixes

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -1,37 +1,37 @@
 #!/usr/bin/env bash
 
-cachedir=~/.cache/rbn
-cachefile=${0##*/}-$1
 
-if [ ! -d $cachedir ]; then
-    mkdir -p $cachedir
-fi
+cachedir="${HOME}/.cache/rbn"
+cachefile="${0##*/}-$1"
 
-if [ ! -f $cachedir/"$cachefile" ]; then
-    touch $cachedir/"$cachefile"
-fi
+# Create cache directory if it doesn't exist
+[ ! -d "$cachedir" ] && mkdir -p "$cachedir"
+
+# Create cache file if it doesn't exist
+[ ! -f "${cachedir}/${cachefile}" ] && touch "${cachedir}/${cachefile}"
 
 # Save current IFS
-SAVEIFS=$IFS
-# Change IFS to new line.
+SAVEIFS="$IFS"
+# Change IFS to new line
 IFS=$'\n'
 
-cacheage=$(($(date +%s) - $(stat -c '%Y' "$cachedir/$cachefile")))
-if [ $cacheage -gt 1740 ] || [ ! -s $cachedir/"$cachefile" ]; then
-    data=($(curl -s https://en.wttr.in/"$1"\?0qnT 2>&1))
-    echo "${data[0]}" | cut -f1 -d, > $cachedir/"$cachefile"
-    echo "${data[1]}" | sed -E 's/^.{15}//' >> $cachedir/"$cachefile"
-    echo "${data[2]}" | sed -E 's/^.{15}//' >> $cachedir/"$cachefile"
+# Calculate cache age
+cacheage=$(( $(date +%s) - $(stat -c '%Y' "${cachedir}/${cachefile}") ))
+
+if [ "$cacheage" -gt 1740 ] || [ ! -s "${cachedir}/${cachefile}" ]; then
+    mapfile -t data < <(curl -s "https://en.wttr.in/${1}?0qnT" 2>&1)
+    echo "${data[0]}" | cut -f1 -d, > "${cachedir}/${cachefile}"
+    echo "${data[1]}" | sed -E 's/^.{15}//' >> "${cachedir}/${cachefile}"
+    echo "${data[2]}" | sed -E 's/^.{15}//' >> "${cachedir}/${cachefile}"
 fi
 
-weather=($(cat $cachedir/"$cachefile"))
+mapfile -t weather < "${cachedir}/${cachefile}"
 
-# Restore IFSClear
-IFS=$SAVEIFS
+# Restore IFS
+IFS="$SAVEIFS"
 
+# Process temperature
 temperature=$(echo "${weather[2]}" | sed -E 's/([[:digit:]])+\.\./\1 to /g')
-
-#echo ${weather[1]##*,}
 
 # https://fontawesome.com/icons?s=solid&c=weather
 case $(echo "${weather[1]##*,}" | tr '[:upper:]' '[:lower:]') in
@@ -70,7 +70,7 @@ case $(echo "${weather[1]##*,}" | tr '[:upper:]' '[:lower:]') in
         ;;
     *)
         condition=""
-        echo -e "{\"text\":\""$condition"\", \"alt\":\"""${weather[0]}""\", \"tooltip\":\"""${weather[0]}": "$temperature" "${weather[1]}""\"}"
+        echo -e "{\"text\":\"${condition}\", \"alt\":\"${weather[0]}\", \"tooltip\":\"${weather[0]}: ${temperature} ${weather[1]}\"}"
         ;;
 esac
 
