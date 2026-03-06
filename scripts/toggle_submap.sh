f9c1d48002cef582f6eb8779ce703b70fd103fbd
Leon Connor Holm <62796852+quantumfate@users.noreply.github.com>
2026-03-06 14:30:19 +0100

    Fixed submap toggle

--- scripts/toggle_submap.sh
+++ scripts/toggle_submap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Toggles a submap and updates the shared ressource for applications that rely on nesting submaps
+# Toggles a submap and updates the shared resource for applications that rely on nesting submaps
 toggle=$1
 IFS=':' read -r app_name next_submap prev_submap <<<$2
 
@@ -14,7 +14,7 @@ if [[ $toggle == "--off" ]]; then
   if [[ $prev_submap == "reset" ]]; then
     notify-send --app-name="Hyprland" -h string:x-canonical-private-synchronous:sys-notify -u low "Submap was reset"
   else
-    notify-send --app-name="$app_name" -h string:x-canonical-private-synchronous:sys-notify -u low "Submap $prev_submap on"
+    notify-send --app-name="$app_name" -h string:x-canonical-private-synchronous:sys-notify -u low "Submap $prev_submap off"
   fi
   hyprctl -q dispatch submap $prev_submap
   echo "$prev_submap" >$XDG_RUNTIME_DIR/hypr/alttab/submap
